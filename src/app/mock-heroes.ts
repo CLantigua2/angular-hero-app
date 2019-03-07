3d734ec864c544febf811b8f80c46269fa5b99f6
Carlos Lantigua <clantigua954@gmail.com>
2019-03-06 22:40:31 -0500

    added styling to heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,21 +1,21 @@
-import { Hero } from "./hero"
+import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 1, name: "Mr. Nice" },
-  { id: 2, name: "Narco" },
-  { id: 3, name: "Bombasto" },
-  { id: 4, name: "Celeritas" },
-  { id: 5, name: "Magneta" },
-  { id: 6, name: "Rubberman" },
-  { id: 7, name: "Dynama" },
-  { id: 8, name: "DR IQ" },
-  { id: 9, name: "Magma" },
-  { id: 10, name: "Tornado" },
-  { id: 11, name: "The Lightning" },
-  { id: 12, name: "OctaScrew" },
-  { id: 13, name: "Reactor" },
-  { id: 14, name: "Node Man" },
-  { id: 15, name: "Captain Captin" },
-  { id: 16, name: "Overload" },
-  { id: 17, name: "Dark Hand" }
-]
+  { id: 1, name: 'Mr. Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'Rubberman' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'DR IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 10, name: 'Tornado' },
+  { id: 11, name: 'The Lightning' },
+  { id: 12, name: 'OctaScrew' },
+  { id: 13, name: 'Reactor' },
+  { id: 14, name: 'Node Man' },
+  { id: 15, name: 'Captain Captin' },
+  { id: 16, name: 'Overload' },
+  { id: 17, name: 'Dark Hand' }
+];
